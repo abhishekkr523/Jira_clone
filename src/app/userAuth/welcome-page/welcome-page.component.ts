43b9e2a29a6324057f1086b57d17523da3cd3223
Krishna <krai100200@gmail.com>
2024-07-22 17:02:36 +0530

    commit

--- src/app/userAuth/welcome-page/welcome-page.component.ts
+++ src/app/userAuth/welcome-page/welcome-page.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { PopupDialogComponent } from './popup-dialog/popup-dialog.component';
 
 @Component({
   selector: 'app-welcome-page',
@@ -6,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrl: './welcome-page.component.css'
 })
 export class WelcomePageComponent {
+  constructor(private dialog: MatDialog) {}
 
+  openDialog(): void {
+    const dialogRef = this.dialog.open(PopupDialogComponent, {
+      width: '1100px',
+      height: '650px',
+      maxWidth:'none',
+      panelClass: 'custom-dialog-container',
+      data: { name: '', email: '' }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      console.log('Form data:', result);
+    });
+  }
 }
