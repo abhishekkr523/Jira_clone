f346089d17c582cea52bea807aa7123137e934c2
Krishna <krai100200@gmail.com>
2024-07-22 17:04:18 +0530

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PopupDialogComponent } from './userAuth/welcome-page/popup-dialog/popup-dialog.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'popup',
+    component:PopupDialogComponent,
+    
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
