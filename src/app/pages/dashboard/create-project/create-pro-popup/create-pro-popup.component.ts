a269d15a2a61175c96752b3c2ebfe5f5bdb2680c
Krishna <krai100200@gmail.com>
2024-07-23 18:07:56 +0530

    ....

---
+++ src/app/pages/dashboard/create-project/create-pro-popup/create-pro-popup.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-pro-popup',
+  templateUrl: './create-pro-popup.component.html',
+  styleUrl: './create-pro-popup.component.css'
+})
+export class CreateProPopupComponent {
+
+}
---
+++ src/app/pages/dashboard/create-project/create-project.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CreateProPopupComponent } from './create-pro-popup/create-pro-popup.component';
+
+
+
+@NgModule({
+  declarations: [
+    CreateProPopupComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CreateProjectModule { }
